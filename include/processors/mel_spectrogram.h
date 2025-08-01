91949fe349bc64bdcf874f73f83c533623ebc858
Blaze Burgess <blaze.i.burgess@gmail.com>
2025-08-01 18:52:48 -0400

    Remove `ThreadSafeBuffer` from `mel_spectrogram.h`, use extracted code
    - Removed the `ThreadSafeBuffer` implementation from `mel_spectrogram.h` to streamline the file.
    - Replaced with an include directive for `core/thread_safe_buffer.h` to centralize and reuse the buffer implementation.
    - Cleaned up unused includes like `<condition_variable>` and `<mutex>`.

--- include/processors/mel_spectrogram.h
+++ include/processors/mel_spectrogram.h
@@ -1,84 +1,16 @@
 #ifndef OPENWAKEWORD_MEL_SPECTROGRAM_H
 #define OPENWAKEWORD_MEL_SPECTROGRAM_H
 
-#include <condition_variable>
 #include <filesystem>
 #include <memory>
-#include <mutex>
 #include <vector>
 #include "core/audio_processor.h"
 #include "core/model_wrapper.h"
 #include "core/types.h"
+#include "core/thread_safe_buffer.h"
 #include "utils/config.h"
 
 namespace openwakeword {
-
-// Thread-safe buffer for inter-processor communication
-template<typename T>
-class ThreadSafeBuffer {
-public:
-    void push(const std::vector<T>& data) {
-        std::unique_lock<std::mutex> lock(mutex_);
-        buffer_.insert(buffer_.end(), data.begin(), data.end());
-        ready_ = true;
-        cv_.notify_one();
-    }
-    
-    void push(const T* data, size_t count) {
-        std::unique_lock<std::mutex> lock(mutex_);
-        buffer_.insert(buffer_.end(), data, data + count);
-        ready_ = true;
-        cv_.notify_one();
-    }
-    
-    std::vector<T> pull(size_t maxCount = 0) {
-        std::unique_lock<std::mutex> lock(mutex_);
-        cv_.wait(lock, [this] { return ready_ || exhausted_; });
-        
-        if (exhausted_ && buffer_.empty()) {
-            return {};
-        }
-        
-        std::vector<T> result;
-        if (maxCount == 0 || maxCount >= buffer_.size()) {
-            result = std::move(buffer_);
-            buffer_.clear();
-        } else {
-            result.assign(buffer_.begin(), buffer_.begin() + maxCount);
-            buffer_.erase(buffer_.begin(), buffer_.begin() + maxCount);
-        }
-        
-        if (!exhausted_) {
-            ready_ = false;
-        }
-        
-        return result;
-    }
-    
-    void setExhausted(bool exhausted) {
-        std::unique_lock<std::mutex> lock(mutex_);
-        exhausted_ = exhausted;
-        ready_ = true;
-        cv_.notify_one();
-    }
-    
-    bool isExhausted() const {
-        std::unique_lock<std::mutex> lock(mutex_);
-        return exhausted_ && buffer_.empty();
-    }
-    
-    size_t size() const {
-        std::unique_lock<std::mutex> lock(mutex_);
-        return buffer_.size();
-    }
-    
-private:
-    mutable std::mutex mutex_;
-    std::condition_variable cv_;
-    std::vector<T> buffer_;
-    bool ready_ = false;
-    bool exhausted_ = false;
-};
 
 // Mel spectrogram processor
 class MelSpectrogramProcessor : public TransformProcessor<AudioFloat, AudioFloat> {
